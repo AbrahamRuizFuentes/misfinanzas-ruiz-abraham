4a2f9d03e3a17bd227947441e980ff8ed4125347
abraham ruiz <abrajhamruizfuentes@gmail.com>
2022-08-29 01:04:17 -0400

    109

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :incomes
   devise_for :usuarios
   root to: "sites#index"
+
+  get '/index2',to:'sites#index2'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
